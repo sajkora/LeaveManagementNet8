e73dfe822b3289f6528b28c787657ac78545349f
sajkora <supcioadi@gmail.com>
2024-04-21 14:07:08 +0200

    Leave types and index customization

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave type name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name="Default number of days")]
+        [Required]
+        public int DefaultDays { get; set; }
+    }
+}
--- LeaveManagement.Web/Program.cs
+++ LeaveManagement.Web/Program.cs
@@ -1,6 +1,10 @@
 using LeaveManagement.Web.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using LeaveManagement.Web.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +16,10 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAutoMapper(typeof(MapperConfig)); ;
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
